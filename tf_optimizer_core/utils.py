29514fbae7bb07acc216bc4b89a20e2980713e84
Luca Giovannesi <l.giovannesi@gmail.com>
2023-06-12 17:22:19 +0200

    Removed prints

--- tf_optimizer_core/utils.py
+++ tf_optimizer_core/utils.py
@@ -1,20 +1,9 @@
-import sys
 import random
 import string
 import zipfile
 import tempfile
 import shutil
 import os
-
-
-def file_callback(blocks: int, block_size: int, size: int):
-    blocks_amount = size / block_size
-    progress = 100 * blocks / blocks_amount
-    progress_str = "{0:.2f}%".format(progress)
-    print(f"\rUploading: {progress_str}", end="")
-    sys.stdout.flush()
-    if blocks >= size // block_size:
-        print()
 
 
 def get_random_string(length) -> str:
@@ -26,10 +15,7 @@ def unzip_file(zipped_path, model_path):
     path = tempfile.mkdtemp()
     with zipfile.ZipFile(zipped_path, "r") as zip:
         zip.extractall(path=path)
-    print(f"TEMP DIR {path}")
-    print(os.listdir(path))
     model_file = os.listdir(path)[0]
     full_path = os.path.join(path, model_file)
-    print(f"MODEL PATH {full_path}")
     shutil.move(full_path, model_path)
     shutil.rmtree(path)
